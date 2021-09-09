d61122c55a891f8a515a71b2c3340cac6c518214
Neha Kennard <43078984+nnkennard@users.noreply.github.com>
2021-09-09 11:14:42 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
+python3 -m venv ve # This only works with Python 3.8 and not 3.9, idk why
+source ve/bin/activate
+python -m pip install numpy # I don't know why this needs to be installed separately, but it does
+python -m pip install -r mini_requirements.txt
 python -m spacy download en_core_web_sm
+jupyter notebook
